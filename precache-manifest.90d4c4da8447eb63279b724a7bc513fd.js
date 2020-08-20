4d6063b43c3b94d9196858673bc174ef27be8a0c
Blacklotus0310 <bloodlotus0310@gmail.com>
2020-08-20 13:30:37 +0700

    Updates

---
+++ precache-manifest.90d4c4da8447eb63279b724a7bc513fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fefd090f6bf3b4efa0c358d3e4aa88e6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2690dd07945fd5ef5ea3",
+    "url": "/Portfolio/static/css/main.1d6081c7.chunk.css"
+  },
+  {
+    "revision": "cc46e1dda7002cc4e13e",
+    "url": "/Portfolio/static/js/2.eb8025ea.chunk.js"
+  },
+  {
+    "revision": "e980a7f3cee8e03adfb625e54adfa237",
+    "url": "/Portfolio/static/js/2.eb8025ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2690dd07945fd5ef5ea3",
+    "url": "/Portfolio/static/js/main.5dc1c435.chunk.js"
+  },
+  {
+    "revision": "944ef253c221e3a128c1",
+    "url": "/Portfolio/static/js/runtime-main.cf2ccc42.js"
+  }
+]);
